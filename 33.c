b8f4ca48acb1969e1c4c3b7d31c151017f2957b3
Josh Oldenburg <joshthegeek@me.com>
2014-12-15 17:31:02 -0500

    Fix question number in problem 33.

--- 33.c
+++ 33.c
@@ -1,5 +1,5 @@
 /*
- * Project Euler #13 by Josh Oldenburg, 12/14/2014
+ * Project Euler #33 by Josh Oldenburg, 12/14/2014
  *
  * The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting to simplify it may incorrectly believe that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
  *
